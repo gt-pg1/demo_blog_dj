700a674ce562b9e51bd45f7e5799caa62428a3fb
gt-pg1 <shot66766@gmail.com>
2023-05-17 12:42:10 +0400

    Added documentation for admin.py

--- blogblog/blog/admin.py
+++ blogblog/blog/admin.py
@@ -10,7 +10,9 @@ from .models import Content, Author, Comment
 
 class UserProfileInline(admin.StackedInline):
     """
-    Defining the mapping of the model (fields) Author as an inline model
+    Inline model definition for the Author model.
+
+    Defines the mapping of the Author model fields as an inline model.
     """
     model = Author
     can_delete = False
@@ -18,8 +20,15 @@ class UserProfileInline(admin.StackedInline):
 
 class AuthorAdmin(UserAdmin):
     """
-    In "inlines" it is specified that the fields of the Author and User models are displayed in the form and edited at the same time
-    "add_form" and "add_fieldsets" define the form for adding a new user
+    Custom UserAdmin for managing authors.
+
+    In "inlines", it is specified that the fields of the Author and User models
+    are displayed in the form and edited at the same time.
+
+    "add_form" and "add_fieldsets" define the form for adding a new user.
+
+    Overrides the get_fieldsets method to hide the Permissions block for all users
+    except superusers.
     """
     inlines = [UserProfileInline]
     add_form = AdminUserCreationForm
@@ -31,7 +40,17 @@ class AuthorAdmin(UserAdmin):
 
     def get_fieldsets(self, request, obj=None):
         """
-        Hides the Permissions block for all users except superusers
+        Returns the fieldsets for the UserAdmin form.
+
+        This method overrides the default implementation to hide the Permissions block
+        for all users except superusers.
+
+        Args:
+            request (HttpRequest): The current HTTP request.
+            obj (User, optional): The User object being edited, or None if it's a new User.
+
+        Returns:
+            list: The list of fieldsets to be displayed in the UserAdmin form.
         """
         fieldsets = super().get_fieldsets(request, obj)
         if not request.user.is_superuser:
@@ -43,8 +62,27 @@ admin.site.unregister(User)
 admin.site.register(User, AuthorAdmin)
 
 
+class CommentInline(admin.TabularInline):
+    """
+    Inline model definition for the Comment model.
+
+    Defines the mapping of the Comment model fields as an inline model.
+    """
+    model = Comment
+    extra = 0
+
+
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin class for managing Content model in the admin panel.
+
+    Sets the display options for the Content model on the admin panel.
+
+    In "inlines", it is specified that the fields of the Comment and Content models
+    are displayed in the form and edited at the same time.
+    """
+    inlines = [CommentInline]
     list_display = ['title',
                     'short_text',
                     'date_time_create',
@@ -57,6 +95,11 @@ class ContentAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    ModelAdmin class for managing Comment model in the admin panel.
+
+    Sets the display options for the Comment model on the admin panel.
+    """
     list_display = ['short_text',
                     'author',
                     'content_link',
@@ -64,6 +107,15 @@ class CommentAdmin(admin.ModelAdmin):
     list_per_page = 40
 
     def content_link(self, obj):
+        """
+        Generates a link to the post that was commented on.
+
+        Args:
+            obj (Comment): The Comment object.
+
+        Returns:
+            str: HTML link to the post.
+        """
         url = reverse('admin:blog_content_change', args=[obj.content.pk])
         return format_html('<a href="{}">{}</a>', url, obj.content)
 

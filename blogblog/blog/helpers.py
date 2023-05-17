da5724eca1838aef26bfc950fa9eb044c447de08
gt-pg1 <shot66766@gmail.com>
2023-05-17 14:44:13 +0400

    Added documentation for helpers.py; to_latin rewritten by list generator

--- blogblog/blog/helpers.py
+++ blogblog/blog/helpers.py
@@ -2,11 +2,22 @@ from unidecode import unidecode
 
 
 def to_latin(string, replace_char=''):
+    """
+    Transliterates a string to its Latin representation.
+
+    Args:
+        string (str): The string to transliterate.
+        replace_char (str, optional): The character to use as a replacement for non-alphanumeric characters.
+            Defaults to an empty string.
+
+    Returns:
+        str: The transliterated string.
+    """
     transliterated = unidecode(string)
-    latin_string = ''
-    for char in transliterated:
-        if char.isalnum() or char in ['-', '_']:
-            latin_string += char
-        else:
-            latin_string += replace_char
+    latin_string = ''.join(
+        char
+        if char.isalnum() or char in ['-', '_']
+        else replace_char
+        for char in transliterated
+    )
     return latin_string

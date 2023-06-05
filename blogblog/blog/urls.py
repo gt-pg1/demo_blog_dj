236f1f1094d802a9615ba13069c01db296292ead
gt-pg1 <shot66766@gmail.com>
2023-06-05 17:39:21 +0400

    Deleted unused imports

--- blogblog/blog/urls.py
+++ blogblog/blog/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import path
-from django.views.generic import RedirectView
 
 from . import views
 

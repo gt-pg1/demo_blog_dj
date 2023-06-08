6bfcee7dad48ce9fac9d69aaa17dc36c0227c582
gt-pg1 <shot66766@gmail.com>
2023-06-08 10:45:07 +0400

    setting.py removed; settings moved to settings/; Added DJANGO_SETTINGS_MODULE to variable environments to quickly change dev settings for production

--- blogblog/blogblog/settings/base.py
+++ blogblog/blogblog/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,7 +25,6 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['192.168.0.108',
                  '127.0.0.1']
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'blog',
-    'debug_toolbar',
     'tinymce',
 ]
 
@@ -55,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'blogblog.middleware.RemoveSlashMiddleware',
     'blogblog.middleware.TimezoneMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = [

0c6d3a9bf157c4d450415f1b87c70f9f204e8513
gt-pg1 <shot66766@gmail.com>
2023-06-15 12:24:36 +0400

    The character limit has been increased to 2500

--- blogblog/blog/static/blog/js/tinymce-setup.js
+++ blogblog/blog/static/blog/js/tinymce-setup.js
@@ -1,8 +1,8 @@
 function (editor) {{
         editor.on("submit", function (e) {{
             var numChars = tinymce.activeEditor.plugins.wordcount.body.getCharacterCount();
-            if (numChars > 2000) {{
-                alert("Maximum 2000 characters allowed.");
+            if (numChars > 2500) {{
+                alert("Maximum 2500 characters allowed.");
                 e.preventDefault();
                 return false;
             }}
